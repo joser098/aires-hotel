00ed5cd5c504050ab84be96c9a69b95d22fb4fe9
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2026-03-21 22:45:33 -0300

    Add SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 // https://astro.build/config
+const site = process.env.SITE_URL;
+
 export default defineConfig({
+  ...(site ? { site } : {}),
   integrations: [react()]
 });
